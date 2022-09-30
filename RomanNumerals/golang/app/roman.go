95b5803e6b07a45820b70115ef8eac85a32091aa
Iain Mitchell <iainjmitchell@gmail.com>
2022-09-30 10:43:20 +0100

    fully working roman numeral converter

--- RomanNumerals/golang/app/roman.go
+++ RomanNumerals/golang/app/roman.go
@@ -1,5 +1,27 @@
 package app
 
+import (
+	"sort"
+	"strings"
+)
+
 func ToRoman(mappings map[int]string, arabic int) string {
-	return mappings[arabic]
+	var romanNumeral string
+	for _, arabicMapping := range highestArabicFirst(mappings) {
+		occurrences := arabic / arabicMapping
+		if occurrences >= 1 {
+			romanNumeral += strings.Repeat(mappings[arabicMapping], occurrences)
+			arabic -= arabicMapping * occurrences
+		}
+	}
+	return romanNumeral
+}
+
+func highestArabicFirst(mappings map[int]string) []int {
+	highestArabicFirst := make([]int, 0, len(mappings))
+	for key := range mappings {
+		highestArabicFirst = append(highestArabicFirst, key)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(highestArabicFirst)))
+	return highestArabicFirst
 }
--- RomanNumerals/golang/app/roman_test.go
+++ RomanNumerals/golang/app/roman_test.go
@@ -7,19 +7,19 @@ import (
 )
 
 var arabicToRomanMappings = map[int]string{
-	1:    "I",
-	4:    "IV",
-	5:    "V",
-	9:    "IX",
-	10:   "X",
-	40:   "XL",
-	50:   "L",
-	90:   "XC",
-	100:  "C",
-	400:  "CD",
-	500:  "D",
-	900:  "CM",
 	1000: "M",
+	900:  "CM",
+	500:  "D",
+	400:  "CD",
+	100:  "C",
+	90:   "XC",
+	50:   "L",
+	40:   "XL",
+	10:   "X",
+	9:    "IX",
+	5:    "V",
+	4:    "IV",
+	1:    "I",
 }
 
 func FuzzStraightConversions(fuzzyTest *testing.F) {
@@ -30,4 +30,24 @@ func FuzzStraightConversions(fuzzyTest *testing.F) {
 		var expected string = arabicToRomanMappings[testInput]
 		assert.Equal(test, expected, ToRoman(arabicToRomanMappings, testInput))
 	})
+}
+
+func TestTwoReturnsII(test *testing.T) {
+	assert.Equal(test, "II", ToRoman(arabicToRomanMappings, 2))
+}
+
+func TestThreeReturnsIII(test *testing.T) {
+	assert.Equal(test, "III", ToRoman(arabicToRomanMappings, 3))
+}
+
+func TestThirtyReturnsXXX(test *testing.T) {
+	assert.Equal(test, "XXX", ToRoman(arabicToRomanMappings, 30))
+}
+
+func TestThirtyOneReturnsXXXI(test *testing.T) {
+	assert.Equal(test, "XXXI", ToRoman(arabicToRomanMappings, 31))
+}
+
+func Test1999ReturnsMCMXCIX(test *testing.T) {
+	assert.Equal(test, "MCMXCIX", ToRoman(arabicToRomanMappings, 1999))
 }
